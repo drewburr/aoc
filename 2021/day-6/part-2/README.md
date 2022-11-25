fab8801583efdbfee8baeb7e2a627aac0aa5138b
browndr <drew.brown@wwt.com>
2022-11-25 12:02:35 -0600

    Solve day 6

---
+++ 2021/day-6/part-2/README.md
@@ -0,0 +1,14 @@
+# Suppose the lanternfish live forever and have unlimited food and space. Would they take over the entire ocean?
+
+After 256 days in the example above, there would be a total of 26984457539 lanternfish!
+
+How many lanternfish would there be after 256 days?
+
+## Utilities and tools
+
+- VSCode
+- Python 3.11.0
+
+## My solution - in words
+
+I chaged my loop counter from 80 to 256 and it worked perfectly.
---
+++ 2021/day-6/part-2/main.py
@@ -0,0 +1,37 @@
+DATA_FILE = 'input.txt'
+DAYS = 256
+RESET_INDEX = 6
+SPAWN_INDEX = 8
+
+
+def setup():
+    data: list[int] = []
+    for line in open(DATA_FILE, 'r'):
+        # Remove newlines
+        l = line.strip()
+        fishTimers = line.split(',')
+        data = list(map(int, fishTimers))
+
+        # We only want the first line
+        return data
+    raise Exception('Failed to setup.')
+
+def main():
+    timers = [0] * 9
+
+    for t in setup():
+        timers[t] += 1
+
+    for _ in range(DAYS):
+        spawning = timers.pop(0)
+        timers.append(0)
+
+        timers[SPAWN_INDEX] += spawning
+        timers[RESET_INDEX] += spawning
+
+
+    print(sum(timers))
+
+
+if __name__ == "__main__":
+    main()
