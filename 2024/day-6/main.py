4dc90bc70311c602c64ee31c2623229082876093
drewburr <drewburr9@gmail.com>
2024-12-08 14:43:32 -0500

    Continue day 6 part 2

--- 2024/day-6/main.py
+++ 2024/day-6/main.py
@@ -26,28 +26,53 @@ steps = 0
 spaces = set()
 
 
-while (True):
-    spaces.add(f'{pos[0]},{pos[1]}')
+def is_loop(x, y, x_dir, y_dir):
+    pass
 
-    x_dir = direction[0] * (1 if turns % 2 == 0 else 0)
-    y_dir = direction[0] * (0 if turns % 2 == 0 else 1)
-    # print(x_dir, y_dir)
 
-    try:
-        facing = data[pos[0]+x_dir][pos[1]+y_dir]
-    except Exception as e:
-        break
+onscreen = True
+while True:
+    spaces.add(f"{pos[0]},{pos[1]}")
 
-    print(facing, end=' ')
-    print(pos, len(spaces), steps)
+    def get_direction():
+        x_dir = direction[0] * (1 if turns % 2 == 0 else 0)
+        y_dir = direction[0] * (0 if turns % 2 == 0 else 1)
+        return x_dir, y_dir
 
-    if facing != wall:
-        pos[turns % 2] += direction[0]
-        steps += 1
+    def get_facing():
+        x_dir, y_dir = get_direction()
+        return data[pos[0] + x_dir][pos[1] + y_dir]
 
-    else:
-        turns += 1
-        direction.append(direction.pop(0))
-        print('turn  ', len(spaces), steps)
+    def step():
+        try:
+            facing = get_facing()
+        except Exception as e:
+            return False
+
+        print(facing, end=" ")
+        print(pos, len(spaces), steps)
+
+        if facing != wall:
+            pos[turns % 2] += direction[0]
+            steps += 1
+        else:
+            turns += 1
+            direction.append(direction.pop(0))
+            print("turn  ", len(spaces), steps)
+
+        return True
+
+    if onscreen and part == 2:
+        _turns = turns
+        _pos = pos.copy()
+        loop_data = data.copy()
+        loop_spaces = dict()
+
+        x_dir, y_dir = get_direction()
+        id = (2 * x_dir) - y_dir
+
+
+    onscreen = step()
+
 
 print(len(spaces))
