d6a857233e75376c3fd97882c01937c0b8d28419
drewburr <drewburr9@gmail.com>
2024-12-10 01:24:10 -0500

    Continue part 6

--- 2024/day-6/main.py
+++ 2024/day-6/main.py
@@ -1,5 +1,5 @@
 input_file = "input.txt"
-# input_file = "example.txt"
+input_file = "example.txt"
 
 part = 2
 
@@ -23,15 +23,13 @@ pos = [x, y]
 turns = 0
 wall = "#"
 steps = 0
+loops = 0
 spaces = set()
 
-
-def is_loop(x, y, x_dir, y_dir):
-    pass
-
+turn_positions = set()
 
 onscreen = True
-while True:
+while onscreen:
     spaces.add(f"{pos[0]},{pos[1]}")
 
     def get_direction():
@@ -44,6 +42,7 @@ while True:
         return data[pos[0] + x_dir][pos[1] + y_dir]
 
     def step():
+        global steps, turns
         try:
             facing = get_facing()
         except Exception as e:
@@ -57,22 +56,45 @@ while True:
             steps += 1
         else:
             turns += 1
+            turn_positions.add((pos[0], pos[1], turns % 4))
             direction.append(direction.pop(0))
-            print("turn  ", len(spaces), steps)
+            print("turn", len(spaces), steps)
 
         return True
 
     if onscreen and part == 2:
-        _turns = turns
+        # Store state
+        _direction = direction.copy()
+        _data = data.copy()
         _pos = pos.copy()
-        loop_data = data.copy()
-        loop_spaces = dict()
-
+        _turns = turns
+        _turn_positions = turn_positions.copy()
+        _steps = steps
+        # Add wall
         x_dir, y_dir = get_direction()
-        id = (2 * x_dir) - y_dir
+        line = data[pos[0] + x_dir]
+        data[pos[0] + x_dir] = line[: pos[1]] + wall + line[pos[1] + 1 :]
 
+        o_turns = turns
+        turn_limit = 0
+        onscreen = True
+        while onscreen and turns - o_turns < 100:
+            p_turns = turns
+            onscreen = step()
+            if p_turns != turns:
+                if (pos[0], pos[1], turns % 4) in turn_positions:
+                    loops += 1
+                    # Reset state
+                    direction = _direction
+                    data = _data
+                    pos = _pos
+                    turns = _turns
+                    turn_positions = _turn_positions
+                    steps = _steps
+                    break
 
     onscreen = step()
 
 
 print(len(spaces))
+print(loops)
